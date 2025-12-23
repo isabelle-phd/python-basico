6d95140eb161958ad9c9acaf8c9d27d49b9e6c41
isabelle-phd <isabelle.sdgs@gmail.com>
2025-12-23 14:07:00 -0300

    commit

--- exercises/desafios09.py
+++ exercises/desafios09.py
@@ -1,8 +1,4 @@
-#crie um programa que leia o nome completo de uma pessoa e mostre:
-#o nome com todas as letras maiúsculas
-#o nome todas as minúsculas
-#quantas letras ao todo (sem considerar espaços)
-#quantas letras tem o primeiro nome
+
 
 #crie um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados
 
